6770ee455acc4354e389f8d94d007c3cf167ffb8
lepikakut <152447835+lepikakut@users.noreply.github.com>
2024-04-28 11:37:57 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+
+
+        print(f"Random Number: {item}")
+
+    for item in data:
+if __name__ == "__main__":
+    main()
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
+import random
+def generate_random_data():
